32d1e12ba18882047c94836f8df96fc6b9d14732
Jalaj28-hub <jalajpathak604@gmail.com>
2022-08-29 01:36:46 +0530

    initial Commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnStart:Button=findViewById(R.id.btn_start)
+        val etName:AppCompatEditText=findViewById(R.id.et_name)
+
+        btnStart.setOnClickListener{
+            if(etName.text.toString().isEmpty()){
+                Toast.makeText(this, "Please Fill Up Your Name", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent=Intent(this,QuizQuestionActivity::class.java)
+                startActivity(intent)
+                intent.putExtra(Constants.USER_NAME,etName.text.toString())
+                finish()
+            }
+        }
+
+    }
+}
